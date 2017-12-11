e198794d4607a699ef259954df9be95ac8250eeb
m.yabu <yabu@hoge.cc>
2017-12-12 07:52:16 +0900

    change to a simple module

--- src/main/ts/jaconv/katahira.ts
+++ src/main/ts/jaconv/katahira.ts
@@ -1,34 +1,30 @@
+var minHira = 'ぁ'.charCodeAt(0);
+var maxHira = 'ゖ'.charCodeAt(0);
+var minKata= 'ァ'.charCodeAt(0);
+var maxKata = 'ヶ'.charCodeAt(0);
 
-namespace jaconv {
-
-  var minHira = 'ぁ'.charCodeAt(0);
-  var maxHira = 'ゖ'.charCodeAt(0);
-  var minKata= 'ァ'.charCodeAt(0);
-  var maxKata = 'ヶ'.charCodeAt(0);
-
-  /**
-   * 全角ひらがなを全角カタカナに変換します。
-   */
-  export function toKatakana(s : string) {
-    var res = '';
-    for (var i = 0; i < s.length; i += 1) {
-      var cd = s.charCodeAt(i);
-      res += (minHira <= cd && cd <= maxHira)?
-        String.fromCharCode(cd - minHira + minKata) : s.charAt(i);
-    }
-    return res;
+/**
+ * 全角ひらがなを全角カタカナに変換します。
+ */
+export function toKatakana(s : string) {
+  var res = '';
+  for (var i = 0; i < s.length; i += 1) {
+    var cd = s.charCodeAt(i);
+    res += (minHira <= cd && cd <= maxHira)?
+      String.fromCharCode(cd - minHira + minKata) : s.charAt(i);
   }
+  return res;
+}
 
-  /**
-   * 全角カタカナを全角ひらがなに変換します。
-   */
-  export function toHiragana(s : string) {
-    var res = '';
-    for (var i = 0; i < s.length; i += 1) {
-      var cd = s.charCodeAt(i);
-      res += (minKata <= cd && cd <= maxKata)?
-        String.fromCharCode(cd - minKata + minHira) : s.charAt(i);
-    }
-    return res;
+/**
+ * 全角カタカナを全角ひらがなに変換します。
+ */
+export function toHiragana(s : string) {
+  var res = '';
+  for (var i = 0; i < s.length; i += 1) {
+    var cd = s.charCodeAt(i);
+    res += (minKata <= cd && cd <= maxKata)?
+      String.fromCharCode(cd - minKata + minHira) : s.charAt(i);
   }
+  return res;
 }
